3d9ce0c6ac0ca4cfcc53d1af7f29f357545ca17a
arikanfvs <furkanarikantr@gmail.com>
2023-11-26 18:55:22 +0300

    Update.

--- xUnit/Services/CountriesService.cs
+++ xUnit/Services/CountriesService.cs
@@ -1,13 +1,56 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        //country list
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
+
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+            //Validation : countryAddRequest null olamaz.
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            //Validation : countryAddRequest.countryName empty olamaz.
+            if (countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            //Validation : countryAddRequest.countryName List'emizdeki country.Name ile eşleşemez.
+            //foreach (var selectedCountry in _countries)
+            //{
+            //    if (countryAddRequest.CountryName == selectedCountry.CountryName)
+            //    {
+            //        throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            //    }
+            //}
+            if (_countries.Where(temp => temp.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            //CountryAddRequest'i Country nesnesine dönüştürüyoruz.
+            Country country = countryAddRequest.ToCountry();
+
+            //Country için CountryId oluşturuyoruz.
+            country.CountryId = Guid.NewGuid();
+
+            //Country nesnesini Country listesine ekliyoruz.
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
         }
     }
 }
