375e50d6ba27f0b121a5770e5d653d48d12f6ec3
arikanfvs <furkanarikantr@gmail.com>
2023-12-01 11:46:42 +0300

    Update.

---
+++ xUnit/CrudExample/Controllers/PersonsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudExample.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("/")]
+        [Route("persons/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
