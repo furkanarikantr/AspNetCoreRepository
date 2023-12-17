6b12805f30ebce8a0d32b6ae42e8415ea54f2037
furkanarikantr <furkanarikantr@gmail.com>
2023-12-17 14:19:17 +0300

    Update.

--- xUnit - CrudOperations - TagHelpers - EF/CrudExample/Controllers/CountriesController.cs
+++ xUnit - CrudOperations - TagHelpers - EF/CrudExample/Controllers/CountriesController.cs
@@ -1,18 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
 
 namespace CrudExample.Controllers
 {
     [Route("[controller]")]
     public class CountriesController : Controller
     {
-        public IActionResult Index()
+        private readonly ICountriesService _countriesService;
+
+        public CountriesController(ICountriesService countriesService)
+        {
+            _countriesService = countriesService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> UploadFromExcel()
         {
             return View();
         }
 
+        [HttpPost]
         [Route("[action]")]
-        public IActionResult UploadFromExcel()
+        public async Task<IActionResult> UploadFromExcel(IFormFile excelFile)
         {
+            if (excelFile == null || excelFile.Length == 0)
+            {
+                ViewBag.ErrorMessage = "Please select an xlsx file!";
+                return View();
+            }
+
+            if (!Path.GetExtension(excelFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                ViewBag.ErrorMessage = "Unsupported file, please select an xlsx file!";
+                return View();
+            }
+
+            int countriesCountInserted = await _countriesService.UploadCountriesFromExcelFile(excelFile);
+
+            ViewBag.Message = $"{countriesCountInserted} Countries Upload!";
+
             return View();
         }
     }
