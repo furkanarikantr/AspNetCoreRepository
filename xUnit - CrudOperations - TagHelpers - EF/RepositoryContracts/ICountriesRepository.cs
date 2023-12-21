56f2272d47b424ce84cb197fc9c386be35727ffb
furkanarikantr <furkanarikantr@gmail.com>
2023-12-21 14:55:01 +0300

    Update.

---
+++ xUnit - CrudOperations - TagHelpers - EF/RepositoryContracts/ICountriesRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryContracts
+{
+    public interface ICountriesRepository
+    {
+        Task<Country> Add(Country country);
+        Task<List<Country>> GetAllCountries();
+        Task<Country?> GetCountryByCountryID(Guid countryID);
+        Task<Country?> GetCountryByCountryName(string countryName);
+    }
+}
---
+++ xUnit - CrudOperations - TagHelpers - EF/RepositoryContracts/IPersonRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryContracts
+{
+    public interface IPersonRepository
+    {
+        Task<Person> AddPerson(Person person);
+
+        Task<List<Person>> GetAllPersons();
+
+        Task<List<Person>> GetPersonByPersonID(Guid personID);
+
+        Task<List<Person>> GetFilteredPersons(Expression<Func<Person, bool>> predicate);
+
+        Task<bool> DeletePersonByPersonID(Guid personID);
+
+        Task<Person> UpdatePerson(Person person);
+    }
+}
